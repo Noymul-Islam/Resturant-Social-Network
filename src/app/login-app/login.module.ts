be802f106976174f20b91d06d782d35ec7634584
Noymul-Islam <shhoroth125@gmail.com>
2018-05-18 23:50:54 +0600

    Log in app UI ready

--- src/app/login-app/login.module.ts
+++ src/app/login-app/login.module.ts
@@ -4,8 +4,8 @@ import {MatButton} from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-
-
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LoginComponent } from './Components/login-component/login.component';
 
 
@@ -17,7 +17,9 @@ import { LoginComponent } from './Components/login-component/login.component';
     BrowserModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    BrowserAnimationsModule
  
   ],
   providers: [],
